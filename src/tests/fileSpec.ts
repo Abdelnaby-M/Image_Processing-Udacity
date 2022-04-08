b3cddc81a96f472ee401b69414f0a79b1540481b
Marwan Abdelnaby <marwan.magdy37@gmail.com>
2022-04-08 17:46:46 +0200

    fileSpecs updated

--- src/tests/fileSpec.ts
+++ src/tests/fileSpec.ts
@@ -21,6 +21,25 @@ describe('Test image processing via sharp', (): void => {
     expect(error).not.toBeNull();
   });
 
+  it('succeeds to write resized thumb file (existing file, valid size values)', async (): Promise<void> => {
+    await File.createThumb({ filename: 'fjord', width: '99', height: '99' });
+
+    const resizedImagePath: string = path.resolve(
+      File.imagesThumbPath,
+      `fjord-99x99.jpg`
+    );
+    let errorFile: null | string = '';
+
+    try {
+      await fs.access(resizedImagePath);
+      errorFile = null;
+    } catch {
+      errorFile = 'File was not created';
+    }
+
+    expect(errorFile).toBeNull();
+  });
+
 });
 
 // Erase test file. Test should not run on productive system to avoid cache loss
